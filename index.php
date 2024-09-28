b94c79e7614a4dfbfdea278992331d1b121d6e2d
Riccardo-Mauri <riccardo90mauri@hotmail.it>
2024-09-28 10:27:09 +0200

    esercizio base eseguito

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Hotel</title>
+    <!--impotazione di bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+<?php
+
+$hotels = [
+
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+
+];
+?>
+
+<!-- stampo l'array e le sue chiavi in html con bootsrtap sotto forma di tabella-->
+<table class="table table-striped">
+    <thead>
+        <tr>
+            <th>Nome</th>
+            <th>Descrizione</th>
+            <th>Parcheggio</th>
+            <th>Voto</th>
+            <th>Distanza dal Centro</th>
+        </tr>
+    </thead>
+    <tbody>
+<?php foreach ($hotels as $datas){ ?>
+    <tr>
+    <td><?php echo  $datas['name'];?></td>
+    <td><?php echo $datas['description'];?></td>
+    <td><?php echo $datas['parking'] ? 'Disponibile' : 'Non Disponibile' ;?></td>
+    <td><?php echo $datas['vote'];?></td>
+    <td><?php echo $datas['distance_to_center'];?></td>
+    </tr>
+<?php } ?>
+    </tbody>
+</table>
+</body>
+</html>
